768475b2f6cf2d436d233bb14793a02fb402eb51
appgp05 <alberto.puig.parellada@gmail.com>
2025-02-27 08:27:54 +0100

    27-02-2025 8:27

---
+++ EjemploCreatePackage.sql
@@ -0,0 +1,5 @@
+-- Son objetos de la base de datos, necesitan nombre único
+
+CREATE OR REPLACE PACKAGE PRUEBA IS
+   PROCEDURE PROC_PRUEBA;
+END PRUEBA;
---
+++ EjemploPruebaPackage&Video.sql
@@ -0,0 +1,8 @@
+--https://www.youtube.com/watch?v=kpuNca-CX_g
+
+SET SERVEROUTPUT ON;
+DECLARE
+BEGIN
+  PRUEBA.PROC_PRUEBA();
+END;
+/
