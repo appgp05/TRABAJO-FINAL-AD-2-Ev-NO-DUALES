1763e78ac4889d4fa498d248017ee8cae054f060
appgp05 <alberto.puig.parellada@gmail.com>
2025-03-12 20:39:06 +0100

    12-03-2025 20:39

---
+++ Entregables/README.txt
@@ -0,0 +1,6 @@
+Explicación de los ficheros entregados:
+ - tablas.sql                           Fichero de creación de las tablas básicas necesarias
+ - BILIOTK_PACKAGE.SQL                  Fichero con el package de los nombres de las funciones obligatorias
+ - BIBLIOTK_BODY.SQL                    Fichero con el package del cuerpo de las funciones obligatorias
+ - BIBLIOTK_ESQUEMA_PARTE2.SQL          Fichero con Las tablas y funciones opcionales
+ - TRIGGERS_Y_PRUEBAS.SQL               Fichero con todos los tiggers, tablas de historial y batería de pruebas
--- Inicios/inicio.sql
+++ Inicios/inicio.sql
@@ -1,6 +1,6 @@
--- ###################################################################################
--- INICIO CON LAS TABLAS NECESARIAS PARA QUE FUNCIONEN LOS PACKAGE, TRIGGERS Y PRUEBAS
--- ###################################################################################
+-- ######
+-- INICIO
+-- ######
 
 ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE;
 
@@ -14,62 +14,3 @@ LOLO
 LOLO
 
 SET SERVEROUTPUT ON;
-
-DROP TABLE sanciones;
-DROP TABLE tipo_sancion;
-
-DROP TABLE autor_obra;
-DROP TABLE autor;
-DROP TABLE prestamo;
-DROP TABLE socio;
-DROP TABLE ejemplar;
-DROP TABLE edicion;
-DROP TABLE obra;
-DROP TABLE historico_obra_auditado;
-
-CREATE TABLE obra (
-    id CHAR(5),
-    titulo VARCHAR(100),
-    anyo INTEGER,
-    CONSTRAINT PK_obra PRIMARY KEY (id),
-    CONSTRAINT NN_titulo CHECK (titulo IS NOT NULL)
-);
-
-CREATE TABLE autor (
-    id CHAR(4),
-    nombre VARCHAR(30),
-    apellidos VARCHAR(60),
-    nacimiento DATE,
-    CONSTRAINT PK_autor PRIMARY KEY (id),
-    CONSTRAINT NN_nombre CHECK (nombre IS NOT NULL),
-    CONSTRAINT NN_apellidos CHECK (apellidos IS NOT NULL)
-);
-
-CREATE TABLE autor_obra (
-    id_autor CHAR(4),
-    id_obra CHAR(5),
-    CONSTRAINT PK_autor_obra PRIMARY KEY (id_autor, id_obra),
-    CONSTRAINT FK_autor_obra_id_autor FOREIGN KEY (id_autor) REFERENCES autor(id), 
-    CONSTRAINT FK_autor_obra_id_obra FOREIGN KEY (id_obra) REFERENCES obra(id)
-);
-
-CREATE TABLE edicion (
-    id CHAR(6),
-    id_obra CHAR(5),
-    isbn VARCHAR(20),
-    anyo INTEGER,
-    CONSTRAINT PK_edicion PRIMARY KEY (id),
-    CONSTRAINT NN_id_obra CHECK (id_obra IS NOT NULL),
-    CONSTRAINT NN_isbn CHECK (isbn IS NOT NULL),
-    CONSTRAINT FK_edicion FOREIGN KEY (id_obra) REFERENCES obra(id)
-);
-
-CREATE TABLE ejemplar (
-    id_edicion CHAR(6),
-    numero INTEGER,
-    alta DATE,
-    baja DATE,
-    CONSTRAINT PK_ejemplar PRIMARY KEY (id_edicion, numero),
-    CONSTRAINT FK_ejemplar FOREIGN KEY (id_edicion) REFERENCES edicion(id),
-    CONSTRAINT NN_alta CHECK (alta IS NOT NULL)
-);
